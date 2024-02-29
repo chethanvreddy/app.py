64b8a82d7e372c61ebe6661b5b12ad33faef51ac
Chethan V <154495745+chethanvreddy@users.noreply.github.com>
2024-02-29 20:40:07 +0530

    app.py

--- app.py
+++ app.py
@@ -265,8 +265,6 @@ for i in Personal:
 In this project, I performed unsupervised clustering. I did use dimensionality reduction followed by agglomerative clustering. I came up with 4 clusters and further used them in profiling customers in clusters according to their family structures and income/spending. This can be used in planning better marketing strategies.
 """
 
-pip install streamlit
-
 import streamlit as st
 
 # Commented out IPython magic to ensure Python compatibility.
